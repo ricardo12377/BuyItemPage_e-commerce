204499e2282c288b0deff09ffdc94d653128b790
Ricardo Costa <rcostaj00@gmail.com>
2022-01-01 23:44:56 -0300

    second_commit

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import style from './card.module.css'
 import Foto1 from '../../images/image-product-1.jpg'
 import Foto1Thumb from '../../images/image-product-1-thumbnail.jpg'
@@ -16,20 +16,46 @@ function Card() {
 
      const [produto, setProduto] = useState('Foto1')
      const [image, setImage] = useState()
+     const [quantidade, setQuantidade] = useState(0)
+     
+     function Adicionar () {
+         setQuantidade(quantidade +1)
+     }
+
+     function Diminuir () {
+         setQuantidade(quantidade - 1)
+     }
+      
+     function AdicionarCarrinho (e) {
+         e.preventDefault();
+            const item = {quantidade}
+
+            fetch('http://localhost:4000/carrinho', {
+                method:"POST",
+                headers: {"Content-type": "application/json"},
+                body: JSON.stringify(item)
+            })
+            .then(() => console.log('Deu certo'))
+     }
 
     return (
         <div className={style.container}>
             
             <div className={style.produto}>
                 <div className={style.fotomaior}>
-                    {produto == 'Foto1' && <img src={Foto1} className={style.foto1} alt="Foto1" />}
+                    {
+                       produto == 'Foto1' && <img src={Foto1} className={style.foto1} alt="Foto1" />
+                    || produto == 'Foto2' && <img src={Foto2} className={style.foto1} alt="Foto2" />
+                    || produto == 'Foto3' && <img src={Foto3} className={style.foto1} alt="Foto3" />
+                    || produto == 'Foto4' && <img src={Foto4} className={style.foto1} alt="Foto4" />
+                    }
                 </div>
 
                 <div className={style.fotos}>
                     <img src={Foto1Thumb} alt="Foto1" onClick={()=>setProduto('Foto1')}/>
-                    <img src={Foto2Thumb} alt="Foto2" />
-                    <img src={Foto3Thumb} alt="Foto3"/> 
-                    <img src={Foto4Thumb} alt="Foto4" />
+                    <img src={Foto2Thumb} alt="Foto2" onClick={() => setProduto('Foto2')}/>
+                    <img src={Foto3Thumb} alt="Foto3" onClick={()=>setProduto('Foto3')}/> 
+                    <img src={Foto4Thumb} alt="Foto4" onClick={()=>setProduto('Foto4')}/>
                 </div>
 
             </div>
@@ -47,17 +73,19 @@ function Card() {
                 <div className={style.valorAtual}>$125,00 <section>50%</section></div>
                 <div className={style.valorPassado}>$250,00</div>
                 <div className={style.quantidade}>
-                    <div className={style.quadrado} id='primeiro'>
+                    <div className={style.quadrado} id='primeiro' onClick={Diminuir}>
                     <img src={Menos} className={style.diminuir} alt="Diminuir quantidade" />
                     </div>
                     
                     <div className={style.quadrado}>
-                        <di>0</di>
+                        <di>{quantidade}</di>
                     </div>
-                    <div className={style.quadrado}>
+
+                    <div className={style.quadrado} onClick={Adicionar}>
                     <img src={Mais} className={style.aumentar} id='terceiro' alt="Aumentar quantidade" />
                     </div>
-                    <button className={style.buttonComprar} ><img className={style.carrinho} src={Carro}/> Add to cart</button>
+
+                    <button onClick={AdicionarCarrinho} className={style.buttonComprar} ><img className={style.carrinho} src={Carro}/> Add to cart</button>
                 </div>
             </div>
         </div>
--- src/components/layout/navbar/navbar.js
+++ src/components/layout/navbar/navbar.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import style from './navbar.module.css'
 import Foto from  '../../../images/image-avatar.png'
 import {AiOutlineShoppingCart} from 'react-icons/ai'
+import FotoCarrinho from '../../../images/image-product-1-thumbnail.jpg'
+import Lixeira from '../../icons/icon-delete.svg'
 
 function Navbar() {
+
+    const [modal, setModal] = useState(false)
+    const [carrinho, setCarrinho] = useState([])
+
+     
+        useEffect(() => {
+            fetch('http://localhost:4000/carrinho', {
+                method: "GET",
+                headers: {"Content-Type": "application-/json"}
+            })
+            .then((response) => response.json())
+            .then((data) => setCarrinho(data))
+        },[])
+
+   
+       
     return (
         <div className={style.container}>
             <div className={style.parteUm}>
@@ -22,11 +40,38 @@ function Navbar() {
 
             <div className={style.parteDois}>
                 <div>
-                   <AiOutlineShoppingCart size={35}/>
+                   <AiOutlineShoppingCart size={35} onClick={() => setModal(true)}/>
                 </div>
 
                 <img className={style.foto} src={Foto} alt="Ricardo" />
             </div>
+
+            {modal && 
+            <div className={style.containerModal} onClick={() => setModal(false)}>
+            <modal className={style.modal}>
+                <div className={style.modalTitle}>
+                Cart
+                </div>
+                <div className={style.modalBody}>
+                   {carrinho ? carrinho.map((item, index) => {
+                       return <div className={style.finalizarCarrinho}>
+                           <img src={FotoCarrinho} alt="Produto" className={style.fotoProduto}/>
+                       <div>
+                           <div>Fall limited Edition Sneakers</div>
+                           <div>$ 125,00 X {item.quantidade} <div className={style.resultadoFinal}>{item.quantidade * 125}</div></div> 
+                       </div>
+
+                        <div>
+                            <img src={Lixeira} alt="Apagar" />
+                        </div>
+                       </div>
+                   }) : null}
+                   
+                     <button className={style.checkout}>Checkout</button>
+                </div>
+            </modal>
+            </div>
+        }
         </div>
     )
 }
